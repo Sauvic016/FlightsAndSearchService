f73009dda50870eaca819374ec18d259b8b69ab9
Sauvic <sauvicpaulchy@gmail.com>
2023-01-11 00:44:17 +0530

    Added CRUD routes for City

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index");
 
 const setupAndStartServer = async () => {
   // create the express object
@@ -9,6 +10,8 @@ const setupAndStartServer = async () => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", ApiRoutes);
 
   app.listen(PORT, () => {
     console.log(`Server started at Port : ${PORT}`);
